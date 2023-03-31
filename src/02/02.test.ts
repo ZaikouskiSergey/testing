02ccc3d819b699d95f634bc92b85d9039e58a2a0
Sergey <Zaikouski.sergey@gmail.com>
2023-03-31 15:28:18 +0300

    add half second lesson

---
+++ src/02/02.test.ts
@@ -0,0 +1,9 @@
+let city: CityType;
+beforeEach(()=>{
+    city = {
+        title: "New York",
+        houses: [],
+        governmentBuldings:[],
+        citizensNumber:100000,
+    }
+})
---
+++ src/02/02.ts
@@ -0,0 +1,53 @@
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: {
+        streetTitle: string
+        city: {
+            title: string
+            countryTitle: string
+        }
+    }
+    technologies:Array<TechType>
+}
+type TechType={
+    id: number
+    title:string
+}
+
+const student: StudentType= {
+    id: 1,
+    name: "Sergey",
+    age: 36,
+    isActive: false,
+    address: {
+        streetTitle: "Shugaeva, 17B",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        },
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
+
+
