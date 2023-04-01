9375afebb6d1a6e99afb2c83c6655a74505ab2f3
Sergey <Zaikouski.sergey@gmail.com>
2023-04-01 18:09:33 +0300

    third lesson

--- src/03/03.ts
+++ src/03/03.ts
@@ -1,4 +1,5 @@
 import {student, StudentType} from "../02/02";
+import {CityType, GoverType, HouseType} from "../02/02_02";
 
 export const sum = (a: number, b: number) => {
     return a + b
@@ -18,4 +19,25 @@ export function makeStudentActive(st: StudentType) {
 
 export function doesStudentLiveIn(st: StudentType, city: string) {
     return st.address.city.title === city
+}
+
+export const addMoneyToBudget = (gover: GoverType, money: number) => {
+    return gover.budget += money
+}
+
+export const repairHouse = (house: HouseType) => {
+    house.repaired = true;
+}
+
+export function toFireStaff(staff: GoverType, quantity: number) {
+    staff.staffCount -= quantity
+}
+
+export const toHireStaff = (quantityStaff: GoverType, quantityHire: number) => {
+    quantityStaff.staffCount+=quantityHire
+}
+
+export function createMessage(city: CityType){
+    return `Hello ${city.title} citizens. I want you be happy. All ${city.citizensNumber} men!`
+
 }
