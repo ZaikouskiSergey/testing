3cabc052d585a14b1ce56b7156f1d5a9da8946c2
Sergey <Zaikouski.sergey@gmail.com>
2023-04-01 11:40:08 +0300

    testing city

---
+++ src/02/02_02.ts
@@ -0,0 +1,25 @@
+export type GoverType = {
+    type: string
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+export type StreetType = {
+    title: string
+}
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<GoverType>
+    citizensNumber: number
+}
+
